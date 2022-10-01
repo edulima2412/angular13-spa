d8cb6081431dea21634943c0f601462b377dcbc0
Eduardo Lima <carlos.rocha@compasso.com.br>
2022-10-01 09:05:20 -0300

    added features of binding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from "@angular/router";
+import { DataBindingComponent } from "./demos/data-binding/data-binding.component";
 import { AboutComponent } from "./institutional/about/about.component";
 import { ContactComponent } from "./institutional/contact/contact.component";
 import { HomeComponent } from "./navigation/home/home.component";
@@ -8,4 +9,5 @@ export const rootRouterConfig: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'feature-data-binding', component: DataBindingComponent },
 ]
